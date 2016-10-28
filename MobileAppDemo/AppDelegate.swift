8e257cc8b585d3c71d203556637fb5b8815dd1c4
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2016-10-28 08:53:09 +0300

    Firebase added and initialised

--- MobileAppDemo/AppDelegate.swift
+++ MobileAppDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+	{
 		// Override point for customization after application launch.
+		FIRApp.configure()
 		return true
 	}
 
@@ -39,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-	}
+	}	
 
 
 }
