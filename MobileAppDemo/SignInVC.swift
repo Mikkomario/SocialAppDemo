e343cc1ea1bc477d1a493d75c07a9042f2840339
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2016-10-31 10:06:36 +0200

    Facebook authentication implemented and tested

--- MobileAppDemo/SignInVC.swift
+++ MobileAppDemo/SignInVC.swift
@@ -7,20 +7,80 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+import FirebaseAuth
 
 class SignInVC: UIViewController
 {
 
-	override func viewDidLoad() {
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	override func didReceiveMemoryWarning() {
+	override func didReceiveMemoryWarning()
+	{
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	@IBAction func facebookButtonPressed(_ sender: UIButton)
+	{
+		let facebookLogin = FBSDKLoginManager()
+		facebookLogin.logIn(withReadPermissions: ["email"], from: self)
+		{
+			(result, error) in
+			
+			if let error = error
+			{
+				print("AUTH: UNABLE TO AUTHENTICATE WITH FACEBOOK")
+				print("AUTH: \(error)")
+			}
+			else if let result = result
+			{
+				if result.isCancelled
+				{
+					print("AUTH: USER CANCELLED FACEBOOK AUTH")
+				}
+				else
+				{
+					print("AUTH: FACEBOOK AUTH SUCCESS")
+					let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+					self.firebaseAuth(credential)
+				}
+			}
+		}
+	}
+	
+	private func firebaseAuth(_ credential: FIRAuthCredential)
+	{
+		FIRAuth.auth()?.signIn(with: credential)
+		{
+			(user, error) in
+			
+			if let error = error
+			{
+				print("AUTH: UNABLE TO AUTHENTICATE TO FIREBASE")
+				print("AUTH: \(error)")
+			}
+			else
+			{
+				print("AUTH: SUCCESSFULLY AUTHENTICATED WITH FIREBASE")
+			}
+		}
+		
+		/*
+		FIRAuth.signIn(with: credential, completion:
+		{
+			(user, error) in
+			
+			
+			
+		})
+*/
+	}
 }
 
